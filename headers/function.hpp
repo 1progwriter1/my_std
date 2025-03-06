79dba5262708521a4d2125555aa4694c55d4395a
1Progwriter1 <vig130205@mail.ru>
2025-03-06 15:42:50 +0300

    my_std::function ready

--- headers/function.hpp
+++ headers/function.hpp
@@ -10,22 +10,38 @@ template<class R, typename ... Types>
 class function;
 
 template<typename R, typename ... Types>
+class CallableObject
+{
+public:
+    virtual ~CallableObject() {}
+    virtual R call(Types ... args) = 0;
+    virtual std::unique_ptr<CallableObject<R, Types ...>> copy() const = 0;
+};
+
+template<typename Callable, typename R, typename ... Types>
+class CallableObjectImpl : public CallableObject<R, Types ...>
+{
+public:
+    CallableObjectImpl(Callable callable) : func_(callable) {}
+    R call(Types ... args) override { return func_(std::forward<Types>(args)...); }
+    std::unique_ptr<CallableObject<R, Types ...>> copy() const override { return std::make_unique<CallableObjectImpl<Callable, R, Types ...>>(func_); }
+private:
+    Callable func_;
+};
+
+template<typename R, typename ... Types>
 class function<R(Types ...)>
 {
 public:
-    function();
-    function(R (*func_ptr)(Types ...));
+    template<typename Callable>
+    function(Callable callable);
+
     function(const function& other);
     function(function&& other);
 
-    // template<class F>
-    // function(F&& f);
-
-    ~function();
-
     function& operator=(const function& other);
     function& operator=(function&& other);
-    function & operator=(std::nullptr_t);
+    function& operator=(std::nullptr_t);
 
     void swap(function& other);
 
@@ -33,74 +49,64 @@ public:
 
     R operator()(Types ... args);
 
-
 private:
-    R (*func_ptr_)(Types ...) = nullptr;
+    std::unique_ptr<CallableObject<R, Types ...>> func_;
 };
 
 template<typename R, typename ... Types>
-function<R(Types ...)>::function() {}
-
-template<typename R, typename ... Types>
-function<R(Types ...)>::function(R (*func_ptr)(Types ...))
-    :   func_ptr_(func_ptr) {}
+template<typename Callable>
+function<R(Types ...)>::function(Callable callable)
+    :   func_(std::make_unique<CallableObjectImpl<Callable, R, Types ...>>(callable)) {}
 
 template<typename R, typename ... Types>
 function<R(Types ...)>::function(const function& other)
-    :   func_ptr_(other.func_ptr_) {}
+    :   func_(std::move(other.func_->copy())) {}
 
 template<typename R, typename ... Types>
 function<R(Types ...)>::function(function&& other)
-    :   func_ptr_(other.func_ptr_)
-{
-    other.func_ptr_ = nullptr;
-}
-
-template<typename R, typename ... Types>
-function<R(Types ...)>::~function() {}
+    :   func_(std::move(other.func_)) {}
 
 template<typename R, typename ... Types>
 function<R(Types ...)>& function<R(Types ...)>::operator=(const function& other)
 {
-    func_ptr_ = other.func_ptr_;
+    func_ = std::move(other.func_->copy());
     return *this;
 }
 
 template<typename R, typename ... Types>
 function<R(Types ...)>& function<R(Types ...)>::operator=(function&& other)
 {
-    func_ptr_ = other.func_ptr_;
-    other.func_ptr_ = nullptr;
+    func_ = std::move(other.func_);
     return *this;
 }
 
 template<typename R, typename ... Types>
 function<R(Types ...)>& function<R(Types ...)>::operator=(std::nullptr_t)
 {
-    func_ptr_ = nullptr;
+    func_ = nullptr;
     return *this;
 }
 
 template<typename R, typename ... Types>
 void function<R(Types ...)>::swap(function& other)
 {
-    std::swap(func_ptr_, other.func_ptr_);
+    std::swap(func_, other.func_);
 }
 
 template<typename R, typename ... Types>
 function<R(Types ...)>::operator bool() const
 {
-    return func_ptr_ != nullptr;
+    return func_ != nullptr;
 }
 
 template<typename R, typename ... Types>
 R function<R(Types ...)>::operator()(Types ... args)
 {
-    if ( !func_ptr_ )
+    if ( !func_ )
     {
         throw std::runtime_error("function pointer is nullptr");
     }
-    return func_ptr_(args...);
+    return func_->call(std::forward<Types>(args)...);
 }
 
 } // namespace my_std
--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,34 @@ int factorial(int a)
     return (a <= 1) ? 1 : a * factorial(a - 1);
 }
 
+class RandomFunctor
+{
+public:
+    RandomFunctor() {std::srand(time(nullptr));}
+    int operator()()
+    {
+        return std::rand();
+    }
+};
+
 int main()
 {
     my_std::function<int(int)> fact = factorial;
-    my_std::function<int(int)> one_more = std::move(fact);
+    my_std::function<int(int)> one_more(fact);
 
-    std::cout << fact(5) << '\n';
+    int res = one_more(5) % 10;
+    my_std::function<int(int)> another = [&res](int a)
+    {
+        res = 1;
+        return a * res;
+    };
+    std::cout << another(10) << ' ' << res << '\n';
+
+    my_std::function<int()> rand_func = RandomFunctor();
+    another = one_more;
+
+
+    std::cout << one_more(5) << ' ' << fact(5) << ' ' << rand_func() << ' ' << another(4) << '\n';
 
     return 0;
 }
